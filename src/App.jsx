ab6f9038e577f2776368661f1f1818478bf30519
robinsonsilva1 <robinson.silva@seidor.com>
2025-07-02 17:08:16 -0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { Outlet } from 'react-router-dom';
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import AccountsPage from './pages/AccountsPage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-900 text-white">
-      <Navbar />
-      <main className="flex-grow flex items-center justify-center p-6">
-        <Outlet />
-      </main>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-900 text-white flex flex-col">
+        <Navbar />
+        <main className="flex-grow flex justify-center items-start p-6">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/accounts" element={<AccountsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
