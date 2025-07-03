bcd00ae243d7c51d908a46a6999c2d9f7c10f472
robinsonsilva1 <robinson.silva@seidor.com>
2025-07-02 21:04:31 -0400

    app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import InicioPage from "./pages/InicioPage";
-import CuentasPage from "./pages/CuentasPage";
-import ContactoPage from "./pages/ContactoPage";
+ import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+ import InicioPage from './pages/InicioPage';
+ import CuentasPage from './pages/CuentasPage';
+ import ContactoPage from './pages/ContactoPage';
+import Navbar from './components/Navbar';  // ← importa Navbar
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<InicioPage />} />
-        <Route path="/cuentas" element={<CuentasPage />} />
-        <Route path="/contacto" element={<ContactoPage />} />
-      </Routes>
-    </Router>
-  );
-}
+ function App() {
+   return (
+     <Router>
+      <Navbar />   
+       <Routes>
+         <Route path="/" element={<InicioPage />} />
+         <Route path="/cuentas" element={<CuentasPage />} />
+         <Route path="/contacto" element={<ContactoPage />} />
+       </Routes>
+     </Router>
+   );
+ }
 
-export default App;
+ export default App;
