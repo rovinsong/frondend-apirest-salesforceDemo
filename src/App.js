c9af64d02b2edfab8ddb87bc7464639ba1a415ba
robinsonsilva1 <robinson.silva@seidor.com>
2025-07-02 20:33:15 -0400

    Switch back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InicioPage from "./pages/InicioPage";
 import CuentasPage from "./pages/CuentasPage";
 import ContactoPage from "./pages/ContactoPage";
-import './styles/colors.css';
-
-
-
-
 
 function App() {
   return (
     <Router>
       <Routes>
-       <Route path="/" element={<InicioPage />} />
-        <Route path="/cuentaspage" element={<CuentasPage />} />
-        <Route path="/contactoPage" element={<ContactoPage />} />
-
-
+        <Route path="/" element={<InicioPage />} />
+        <Route path="/cuentas" element={<CuentasPage />} />
+        <Route path="/contacto" element={<ContactoPage />} />
       </Routes>
     </Router>
   );
