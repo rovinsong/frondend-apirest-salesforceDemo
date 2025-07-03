15d5d9ed87305d1afc10aed49553bf5c6cd4292b
robinsonsilva1 <robinson.silva@seidor.com>
2025-07-02 21:13:16 -0400

    inicio

--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -1,35 +1,12 @@
-import { useNavigate } from "react-router-dom";
-import { Card } from "primereact/card";
-import { Button } from "primereact/button";
 import React from "react";
 
 function InicioPage() {
-  const navigate = useNavigate();
-  <h1 className="text-3xl font-bold underline text-blue-600">
-  ¡Tailwind está funcionando!
-</h1>
-
   return (
-    <div className="min-h-screen bg-[#F9FAFB] flex items-center justify-center px-4">
-      <Card className="w-full max-w-sm shadow-xl">
-        <h2 className="text-xl font-bold text-center mb-6 text-[#222221]">
-          📚 Menú Principal
-        </h2>
-
-        <div className="flex flex-col gap-4">
-              <Button
-                label="👤 Cuentas"
-                className="bg-[#E0E0E0] text-[#222222] hover:bg-blue-300"
-                onClick={() => navigate("/cuentas")}
-              />
-
-              <Button
-                label="✉️ Contáctanos"
-                className="bg-[#FFD54F] text-[#222222] hover:bg-yellow-500"
-                onClick={() => navigate("/contacto")}
-              />
-        </div>
-      </Card>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      <h1 className="text-5xl font-bold mb-4">Bienvenido a Salesforce Demo</h1>
+      <p className="text-gray-700 text-lg">
+        Usa el menú superior para navegar entre Inicio, Cuentas y Contáctanos.
+      </p>
     </div>
   );
 }
