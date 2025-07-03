4485e645c5586285a40b7c2fb55960ec087cc77b
robinsonsilva1 <robinson.silva@seidor.com>
2025-07-02 20:27:18 -0400

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import InicioPage from "./pages/InicioPage";
 import CuentasPage from "./pages/CuentasPage";
 import ContactoPage from "./pages/ContactoPage";
